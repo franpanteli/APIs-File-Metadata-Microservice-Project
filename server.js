849d6cb5a9f35d9eef5fff1b60cb8a30e3d1e305
Fran Panteli <francescapanteli@gmail.com>
2024-03-02 16:18:26 +0000

    This commit pushes changes which separate out the elements of the project server.js file.

--- server.js
+++ server.js
@@ -1,22 +1,57 @@
 var express = require("express");
 var cors = require("cors");
 require("dotenv").config();
-
 const multer = require("multer");
-
 var app = express();
+
+
+
+
+
+
+
 
 app.use(cors());
 app.use("/public", express.static(process.cwd() + "/public"));
+
+
+
+
+
+
+
+
+
+
 
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
 });
 
+
+
+
+
+
+
+
+
+
 app.post("/api/fileanalyse", multer().single("upfile"), function (req, res) {
   const { originalname, mimetype, size } = req.file;
   return res.json({ name: originalname, type: mimetype, size });
 });
+
+
+
+
+
+
+
+
+
+
+
 
 const port = process.env.PORT || 8080;
 app.listen(port, function () {
